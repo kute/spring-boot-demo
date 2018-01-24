b7a3c23ff0ad3b8cd26b192ca29f6152fc60926e
kute <kutekute00@gmail.com>
2018-01-24 20:22:52 +0800

    thrift update

---
+++ src/main/resources/thrift/Constant.thrift
@@ -0,0 +1,6 @@
+namespace java com.kute.demo.rpc.thrift.constant
+
+include "Types.thrift"
+
+const Types.Double VERSION = 1.0
+const map<string,string> MAP_CONST = {"hello": "world", "goodnight": "moon"}
