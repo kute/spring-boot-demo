8349ab2f2435496d73b14283c4f8eff0a4e4b46e
bailong03 <bailong03@meituan.com>
2018-06-22 16:34:32 +0800

    add powermock dependency

---
+++ src/main/java/com/kute/demo/test/MainTest.java
@@ -0,0 +1,19 @@
+package com.kute.demo.test;
+
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
+/**
+ * created on 2018-05-07 17:16
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
