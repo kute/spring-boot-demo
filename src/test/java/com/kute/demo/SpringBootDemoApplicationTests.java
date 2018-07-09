479c6ab58d9189370e5241b29a63622e4cefa40f
bailong03 <bailong03@meituan.com>
2018-07-09 14:49:06 +0800

    add travis ci

--- src/test/java/com/kute/demo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/kute/demo/SpringBootDemoApplicationTests.java
@@ -1,23 +1,11 @@
 package com.kute.demo;
 
-import com.kute.demo.orm.jdbctemplate.dao.IUserDao;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootDemoApplicationTests {
-
-    @Autowired
-    private IUserDao userDao;
-
-	@Test
-	public void testInsert() {
-        String sql = "insert into User values(3, 'lili', 19)";
-        userDao.execute(sql);
-	}
 
 }
